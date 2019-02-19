afec448046c8c330813964e1a0db8c244451525b
Nongxi <Ironxss@protonmail.com>
2019-02-20 00:35:55 +0800

    spring study notes

---
+++ 09Spring-aop01/src/com/ironxi/test/Test.java
@@ -0,0 +1,10 @@
+package com.ironxi.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args){
+		ApplicationContext ac=new ClassPathXmlApplicationContext("beans.xml");
+	}
+}
